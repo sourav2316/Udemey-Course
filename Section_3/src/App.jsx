1215505b12be942469deb69872c95a4f8d712d30
sourav2316 <ksourav044@gmail.com>
2025-01-15 22:46:03 +0530

    1st Branch test

--- Section_3/src/App.jsx
+++ Section_3/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   const array = ["Sourav", "manish", "vaibhav"];
-
+  //////////////////////////////
   function genRandomNumber(num) {
     return Math.floor(Math.random() * (num + 1));
   }
