393ca8e530acb051c969459e824b61bae8ffb223
sourav2316 <ksourav044@gmail.com>
2025-01-15 22:19:05 +0530

    1st Push test

--- Section_3/src/App.jsx
+++ Section_3/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
     return Math.floor(Math.random() * (num + 1));
   }
 
-  return <h1>{array[genRandomNumber(2)]} Hello World Kida vayi mundeyaaaa</h1>;
+  const descriptions = array[genRandomNumber(2)];
+
+  return <h1>{descriptions} Hello World Kida vayi mundeyaaaa</h1>;
 }
 
 export default App;
