146a0467811ccbff2c646352adaa7ac96e4a7fa0
sourav2316 <ksourav044@gmail.com>
2025-01-28 22:33:23 +0530

    Investment-calculator Project

---
+++ Investment_calculator/src/components/Results.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { calculateInvestmentResults } from "../util/investment";
+import { formatter } from "../util/investment";
+
+const Results = ({ input }) => {
+  const result = calculateInvestmentResults(input);
+  const initialInvestment =
+    result[0].valueEndOfYear - result[0].interest - result[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result?.map((yearData, index) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={index}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Results;
